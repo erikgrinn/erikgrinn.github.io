9fb34d09e4c44f91a6f71bad52669bf71b106fbd
Erik Grinn <grinn.erik@gmail.com>
2024-12-06 20:16:59 -0500

    deployment adjustements

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://erikgrinn.github.io',
     integrations: [
         mdx(),
         sitemap(),
